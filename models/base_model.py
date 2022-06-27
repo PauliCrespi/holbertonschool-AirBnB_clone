9b10632c59d39f537255db8c319f9b89cdd0a0b8
PauliCrespi <4377@holberton.com>
2022-06-27 09:39:43 -0700

    ja

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import datetime
 class BaseModel:
     """class model"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
