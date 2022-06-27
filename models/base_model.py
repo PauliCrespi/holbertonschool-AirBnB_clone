9d3e3d607dc1ed7c7fad6dbbabe4bd1fa952ce7a
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-06-27 09:32:48 -0700

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel:
     def __init__(self):
         """init"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """str representation"""
@@ -18,9 +18,13 @@ class BaseModel:
 
     def save(self):
         """saves"""
-        setattr(self, self.updated_at, datetime.datetime.now().isoformat())
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """returns dictionary of the instance"""
-    
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        dAux = self.__dict__
+        dAux["__class__"] = "BaseModel"
+        return dAux
 
