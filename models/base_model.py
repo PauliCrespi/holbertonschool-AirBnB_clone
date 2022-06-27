486798d94f99fa7b110038c3ddcdddc1c8f0c50c
PauliCrespi <4377@holberton.com>
2022-06-27 09:59:10 -0700

    b

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """init"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """str representation"""
