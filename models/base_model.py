78286cb671f5a14b9afb55017737ce2790cd2c98
PauliCrespi <4377@holberton.com>
2022-07-01 06:40:39 -0700

    base

--- models/base_model.py
+++ models/base_model.py
@@ -4,28 +4,31 @@ import uuid
 import datetime
 import models
 
+
 class BaseModel:
     """class model"""
 
     def __init__(self, *args, **kwargs):
         """init"""
+        date = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at":
-                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        value = datetime.strptime(value, date)
                     elif key == "updated_at":
-                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        value = datetime.strptime(value, date)
                     setattr(self, key, value)
         else:
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
             models.storage.new(self)
 
     def __str__(self):
         """str representation"""
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
         """saves"""
@@ -42,4 +45,3 @@ class BaseModel:
                 dAux[key] = dAux[key].isoformat()
         dAux["__class__"] = self.__class__.__name__
         return dAux
-
