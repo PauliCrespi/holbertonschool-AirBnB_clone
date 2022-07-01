2486d6ed5ee1f6577fc02dc1aefade4ba457a668
PauliCrespi <4377@holberton.com>
2022-07-01 06:32:54 -0700

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,56 @@
 #!/usr/bin/python3
 """File Storage"""
 
+from models.user import User
+from models.base_model import BaseModel
+import json
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+model = {"BaseModel": BaseModel,
+         "User": User,
+         "State": State,
+         "City": City,
+         "Amenity": Amenity,
+         "Place": Place,
+         "Review": Review
+         }
 
 
 class FileStorage:
     """class FileStorage"""
-    def __init__(self, file_path, objects):
-        """inisialization"""
-        self.__file_path = file_path
-        self.__objects = objects
 
-    @property
+    __file_path = "file.json"
+    __objects = {}
+
     def all(self):
         """returns the dictionary __objects"""
-        return self.__file_path
+        return self.__objects
 
-    @new.setter
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-    self.__objects = obj.get('__class__') + "." + obj.get(id)
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        dic_lis = []
-        if list_objs is None:
-            list_objs = []
-        for c in list_objs:
-            dic_lis.append(c.to_dictionary())
-        with open(self.__file_path, 'w', encoding="utf-8") as f:
-            f.write(self.(self.__objects ))
+        json_obj = {}
+        for key in self.__objects:
+            json_obj[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w", encoding="utf=8") as f:
+            json.dump(json_obj, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r", encoding="utf=8") as f:
+                jsob = json.load(f)
+            for key in jsob:
+                self.__objects[key] = model[jsob[key]["__class__"]](**jsob[key])
+        except FileNotFoundError:
+            pass
 
