d328eac39bcda4f8d332a643dfbe08081cb17aee
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-02 17:26:21 -0700

    tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,7 +9,6 @@ from models.base_model import BaseModel
 class testState(unittest.TestCase):
     """tests for State"""
 
-
     def test1(self):
         """tests state name and save"""
         state = State()
