89fd4d08d2f364bf0e0b56620c14864078b20228
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-03 11:10:40 -0700

    tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import unittest
+import models
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class TestFileStorage(unittest.TestCase):
+    """tests for file storage"""
+
+    def test1(self):
+        """test type"""
+        x = FileStorage()
+        self.assertEqual(type(x.all()), dict)
+
+if __name__ == '__main__':
+    unittest.main()
