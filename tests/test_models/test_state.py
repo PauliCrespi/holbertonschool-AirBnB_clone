6b7509e5d65ef2ed5dc97af1e5bfea1326f10072
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-07-02 17:24:59 -0700

    tests

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class testState(unittest.TestCase):
+    """tests for State"""
+
+
+    def test1(self):
+        """tests state name and save"""
+        state = State()
+        state.name = "California"
+        state.save()
+        self.assertEqual(state.name, "California")
+
+
+if __name__ == '__main__':
+    unittest.main()
